c26b6907a686266107a33793a84d68a507882d4f
marzgra <marzgra@o2.pl>
2021-07-08 20:35:14 +0200

    unit tests - fixed

--- src/test/java/pl/rsokol/step2/NotesServiceImplTest.java
+++ src/test/java/pl/rsokol/step2/NotesServiceImplTest.java
@@ -3,14 +3,24 @@ package pl.rsokol.step2;
 import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import pl.rsokol.Note;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.spy;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
 public class NotesServiceImplTest {
+
+    private static final float EQUALITY_DELTA = 0.01f;
+    @Mock
+    private NotesStorage notesStorage;
+    private NotesService notesService;
 
     @Before
     public void before() {
@@ -19,19 +29,36 @@ public class NotesServiceImplTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void add_nullNote() throws Exception {
+    public void shouldNotAddNullNote() throws Exception {
+        // when
         notesService.add(null);
+
+        //then
+        then(notesStorage).should(never()).add(any());
     }
 
     @Test
-    public void add() throws Exception {
-        notesService.add(Note.of("Jan Kowalski", 5.0f));
+    public void shouldAddNoteWhenCorrectNote() throws Exception {
+        // given
+        Note note = Note.of("Jan Kowalski", 5.0f);
+
+        // when
+        notesService.add(note);
+
+        //then
+        then(notesStorage).should(times(1)).add(note);
     }
 
     @Test(expected = IOException.class)
     public void add_persistenceError() throws Exception {
-        Mockito.doThrow(new IOException()).when(notesStorage).add(any(Note.class));
-        notesService.add(Note.of("Jan Kowalski", 5.0f));
+        // given
+        Note note = Note.of("Jan Kowalski", 5.0f);
+
+        Mockito.doThrow(new IOException()).when(notesStorage).add(note);
+
+        // when
+        notesService.add(note);
+
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -41,7 +68,14 @@ public class NotesServiceImplTest {
 
     @Test
     public void averageOf_nonexistent() {
-        assertEquals(0.0f, notesService.averageOf("Maria Curie"), EQUALITY_DELTA);
+        // given
+        final String name = "Maria Curie";
+
+        // when
+        final float avg = notesService.averageOf(name);
+
+        // then
+        assertEquals(0.0f, avg, EQUALITY_DELTA);
     }
 
     @Test
@@ -63,10 +97,5 @@ public class NotesServiceImplTest {
         notesService.clear();
         assertEquals(0.0f, notesService.averageOf("Jan Kowalski"), EQUALITY_DELTA);
     }
-
-    private static final float EQUALITY_DELTA = 0.01f;
-
-    private NotesStorage notesStorage;
-    private NotesService notesService;
 
 }
